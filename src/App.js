689a55e974bf2eedc3e40770820a8ceeff012bd8
Faudhya <garaaudhy@gmail.com>
2023-03-02 19:40:53 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Fruits } from "./fruits";
+import { Container, Input } from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [query, setQuery] = useState("");
+    let fruits = Fruits;
+    let fruitslist = fruits
+        .filter((fruit) => fruit.name.toLowerCase().includes(query))
+        .map((fruit) => {
+            return (
+                <li key={fruit.id} className="list-item">
+                    {fruit.name}
+                </li>
+            );
+        });
+    return (
+        <Container className="container" centerContent>
+            <Input
+                bgColor={"white"}
+                borderColor={"black"}
+                focusBorderColor="black"
+                placeholder="Search"
+                className="search-bar"
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            <ul className="list">{fruitslist}</ul>
+        </Container>
+    );
 }
 
 export default App;
